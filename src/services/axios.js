cdb698a4e8390ea83e038ecff9d5a5786844fed7
jihyun song <43053791+jihyunsong42@users.noreply.github.com>
2023-07-11 18:43:28 +0800

    stash restoration

--- src/services/axios.js
+++ src/services/axios.js
@@ -4,12 +4,10 @@ import Address from "../constants/Address";
 export const getBalance = async (address) => {
   try {
     const res = await axios.get(Address.TESTNET_ACCOUNT_URL2 + address + "/balances");
-    if (res.status === 200) {
-      console.log("GetBalance");
-      console.log(res);
+
+    if (res.status == 200) {
       let balance = parseInt(res.data.stx.balance, 10);
-      console.log(balance);
-      return res.data;  // hexadecimal to decimal
+      return res.data;
     }
     else {
       console.log(res.statusText);
